8b49ead9cc710cb524110c3fe7c0f69c4675a3bc
Sedome <sedomem@gmail.com>
2026-03-25 00:39:09 +0300

    Add files via upload

---
+++ sw_js.js
@@ -0,0 +1,22 @@
+const CACHE = "runclub-v1";
+const ASSETS = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", e =>
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  )
+);
+
+self.addEventListener("activate", e =>
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+);
+
+self.addEventListener("fetch", e =>
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+);
